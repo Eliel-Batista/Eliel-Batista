e2281fe0a1d02db4acfe86de09330e165aa2f44c
Eliel-Batista <eliel.batista.ebs@gmail.com>
2024-06-11 23:35:53 -0300

    sprint 1

---
+++ Sprint 1/Atividade 1/main.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  int idade = 25;
+
+  double altura = 1.77;
+
+  bool geek = true;
+
+  String nome = 'Eliel';
+
+  String apelido = "Joshua, o arrasador";
+
+  print("Eu sou ${apelido} \nmas meu nome é: ${nome}, \neu me considero geek? ${geek} \nEu tenho ${altura} metros de altura e \n${idade} anos de idade") ;
+  
+}
---
+++ Sprint 1/Atividade 2/main.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  int idade = 25;
+
+  double altura = 1.77;
+
+  bool geek = true;
+
+  String nome = 'Eliel';
+
+  String apelido = "Joshua, o arrasador";
+
+  List<dynamic> Eliel_informacoes = [idade, altura, geek, nome, apelido];
+
+  print("Eu sou ${Eliel_informacoes[4]} \nmas meu nome é: ${Eliel_informacoes[3]}, \neu me considero geek? ${Eliel_informacoes[2]} \nEu tenho ${Eliel_informacoes[1]} metros de altura e \n${Eliel_informacoes[0]} anos de idade") ;
+  
+}
